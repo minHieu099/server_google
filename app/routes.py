d16191520d8af550d53931ef4c56b1359b08df36
minhhieu <minhhieuhm0904@gmail.com>
2024-07-12 14:52:12 +0700

    fix_id

--- app/routes.py
+++ app/routes.py
@@ -11,7 +11,7 @@ def get_search_info():
 @main.route('/search_details/<int:info_id>', methods=['GET'])
 def get_search_details(info_id):
     details = SearchDetails.query.filter_by(search_info_id=info_id).limit(3).all()
-    return jsonify([{'link': detail.link, 'title': detail.title, 'keywords_extracted': detail.keywords_extracted, 'confidence_level': detail.confidence_level} for detail in details]), 200, {'Content-Type': 'application/json; charset=utf-8'}
+    return jsonify([{'id':detail.id,'link': detail.link, 'title': detail.title, 'keywords_extracted': detail.keywords_extracted, 'confidence_level': detail.confidence_level} for detail in details]), 200, {'Content-Type': 'application/json; charset=utf-8'}
 
 @main.route('/search_details/url/<int:detail_id>', methods=['GET'])
 def get_detail_url(detail_id):
