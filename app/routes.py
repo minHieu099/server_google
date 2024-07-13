d30658cf60a23808c3ce193cb02f801b2174d930
minhhieu <minhhieuhm0904@gmail.com>
2024-07-13 16:49:33 +0700

    add_data

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 from .models import db, SearchInfo, SearchDetails
-
+import json
 main = Blueprint('main', __name__)
 
 @main.route('/search_info', methods=['GET'])
@@ -11,7 +11,13 @@ def get_search_info():
 @main.route('/search_details/<int:info_id>', methods=['GET'])
 def get_search_details(info_id):
     details = SearchDetails.query.filter_by(search_info_id=info_id).limit(3).all()
-    return jsonify([{'id':detail.id,'link': detail.link, 'title': detail.title, 'keywords_extracted': detail.keywords_extracted, 'confidence_level': detail.confidence_level} for detail in details]), 200, {'Content-Type': 'application/json; charset=utf-8'}
+    return jsonify([{
+        'id': detail.id,
+        'link': detail.link,
+        'title': detail.title,
+        'keywords_extracted': json.loads(detail.keywords_extracted),
+        'confidence_level': detail.confidence_level
+    } for detail in details]), 200, {'Content-Type': 'application/json; charset=utf-8'}
 
 @main.route('/search_details/url/<int:detail_id>', methods=['GET'])
 def get_detail_url(detail_id):
@@ -20,7 +26,7 @@ def get_detail_url(detail_id):
         'link': detail.link,
         'title': detail.title,
         'content': detail.content,
-        'content_processed': detail.content_processed,
-        'keywords_extracted': detail.keywords_extracted,
+        'content_processed': json.loads(detail.content_processed) if detail.content_processed else "",
+        'keywords_extracted': json.loads(detail.keywords_extracted) if detail.keywords_extracted else [],
         'confidence_level': detail.confidence_level
     }), 200, {'Content-Type': 'application/json; charset=utf-8'}
